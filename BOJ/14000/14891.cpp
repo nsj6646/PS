7aca7cb36abd84a8d7594433ac1f792b9476ba84
nsj6646 <nsj6646@gmail.com>
2019-03-29 19:06:29 +0900

    톱니바퀴

--- BOJ/14000/14891.cpp
+++ BOJ/14000/14891.cpp
@@ -1,129 +1,53 @@
-//#include <cstdio>
-//#include <cstring>
-//
-//int gear[5][8];
-//int isrotate[5];
-//void check(int from,int to) {
-//	switch (from)
-//	{
-//	case 1:
-//		if (gear[1][2] != gear[2][6]) {
-//			isrotate[2] = isrotate[1] * -1;
-//		}
-//		break;
-//	case 2:
-//		if (to == 1) {
-//			if (gear[2][6] != gear[1][2]) {
-//				isrotate[1] = isrotate[2] * -1;
-//			}
-//		}
-//		else {
-//			if (gear[2][2] != gear[3][6]) {
-//				isrotate[3] = isrotate[2] * -1;
-//			}
-//		}
-//		break;
-//	case 3:
-//		if (to == 2) {
-//			if (gear[3][6] != gear[2][2]) {
-//				isrotate[2] = isrotate[3] * -1;
-//			}
-//		}
-//		else {
-//			if (gear[3][2] != gear[4][6]) {
-//				isrotate[4] = isrotate[3] * -1;
-//			}
-//		}
-//		break;
-//	case 4:
-//		if (gear[4][6] != gear[4][2]) {
-//			isrotate[3] = isrotate[4] * -1;
-//		}
-//		break;
-//	}
-//}
-//void rotate_r(int x) {
-//	int temp = gear[x][7];
-//	for (int i = 7; i >= 1; i--) {
-//		gear[x][i] = gear[x][i - 1];
-//	}
-//	gear[x][0] = temp;
-//}
-//void rotate_l(int x) {
-//	int temp = gear[x][0];
-//	for (int i = 0; i < 7; i++) {
-//		gear[x][i] = gear[x][i + 1];
-//	}
-//	gear[x][7] = temp;
-//}
-//void simulate(int x, int d) {
-//	isrotate[x] = d;
-//	switch (x)
-//	{
-//	case 1:
-//		check(1,2);
-//		check(2, 3);
-//		check(3, 4);
-//		break;
-//	case 2:
-//		check(2, 1);
-//		check(2, 3);
-//		check(3, 4);
-//		break;
-//	case 3:
-//		check(3, 2);
-//		check(3, 4);
-//		check(2, 1);
-//		break;
-//	case 4:
-//		check(4, 3);
-//		check(3, 2);
-//		check(2, 1);
-//		break;
-//	}
-//	for (int i = 1; i <= 4; i++) {
-//		if (isrotate[i] == 1) {
-//			rotate_r(i);
-//		}
-//		else if (isrotate[i] == -1) {
-//			rotate_l(i);
-//		}
-//	}
-//
-//}
-//void printall() {
-//	for (int i = 1; i <= 4; i++) {
-//		for (int j = 0; j < 8; j++) {
-//			printf("%d ", gear[i][j]);
-//		}
-//		printf("\n");
-//	}
-//	printf("\n");
-//}
-//int calc() {
-//	int ret = 0;
-//	for (int i = 1; i <= 4; i++) {
-//		ret += gear[i][0] << (i - 1);
-//	}
-//	return ret;
-//}
-//int main()
-//{
-//	int k;
-//	scanf("%d", &k);
-//	for (int i = 1; i <= 4; i++) {
-//		for (int j = 0; j < 8; j++) {
-//			scanf("%1d", &gear[i][j]);
-//		}
-//	}
-//
-//
-//	for (int i = 0; i < k; i++) {
-//		memset(isrotate, 0, sizeof(int) * 5);
-//		int gn, rd;
-//		scanf("%d %d", &gn, &rd);
-//		simulate(gn, rd);
-//	}
-//	printf("%d", calc());
-//	return 0;
-//}
+#include <cstdio>
+#include <cstring>
+int a[5][8];        //톱니바퀴 정보 배열
+int pivot[5];       //12시방향에 해당하는 인덱스 정도
+bool visited[5];
+int k;
+
+void run(int idx,int dir){
+    if(idx!=1){
+        if(a[idx-1][(pivot[idx-1]+2)%8]^a[idx][(pivot[idx]+6)%8]){
+            if(!visited[idx-1]){
+                visited[idx-1]=true;
+                run(idx-1,dir*-1);
+            }
+        }
+    }
+
+    if(idx!=4){
+        if(a[idx][(pivot[idx]+2)%8]^a[idx+1][(pivot[idx+1]+6)%8]){
+            if(!visited[idx+1]){
+                visited[idx+1]=true;
+                run(idx+1,dir*-1);
+            }
+        }
+    }
+
+    pivot[idx]=(pivot[idx]-dir+8)%8;
+}
+int main()
+{
+    for(int i=1;i<=4;++i){
+        for(int j=0;j<8;++j){
+            scanf("%1d",&a[i][j]);
+        }
+    }
+    scanf("%d",&k);
+
+    int idx,dir;
+    for(int i=0;i<k;++i){
+        scanf("%d %d",&idx,&dir);
+        memset(visited,0,sizeof(visited));
+        visited[idx]=true;
+        run(idx,dir);
+    }
+    int ans=0;
+    int score=1;
+    for(int i=1;i<=4;++i){
+        if(a[i][pivot[i]]) ans+=score;
+        score<<=1;
+    }
+    printf("%d",ans);
+    return 0;
+}
