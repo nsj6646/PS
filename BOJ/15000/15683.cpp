21d65773f0dd0ad70426876618042fa29e4407c9
nsj6646 <nsj6646@gmail.com>
2019-04-03 00:13:41 +0900

    감시 다시품

--- BOJ/15000/15683.cpp
+++ BOJ/15000/15683.cpp
@@ -1,102 +1,96 @@
-#include <cstdio>
-#include <cstring>
-#include <vector>
+#include <bits/stdc++.h>
+#define SAFE(x,y)	x>=0&&x<n&&y>=0&&y<m
 using namespace std;
 
-struct cctv
+
+struct CCTV
 {
 	int x, y;
-	int num;
+	int type;
 	int dir;
-	cctv(int x = 0, int y = 0, int n = 0,int d=0) :x(x), y(y), num(n),dir(d) {}
+	CCTV(int x,int y,int t,int d):x(x),y(y),type(t),dir(d){}
 };
-int n, m;
+vector<CCTV> v;
 
-vector<cctv> tvs;
-int dx[6][4][4]{	//몇번 카메라가 어느방향을 보고있을 때 x(상:0, 우:1, 하:2, 좌:3)
-	{ {0} },
-	{ { -1,0,0,0 },{ 0,0,0,0 },{ 0,0,1,0 },{ 0,0,0,0 }},	//1
-	{ { 0,0,0,0 },{ -1,0,1,0 }},	//2
-	{ { -1,0,0,0 },{ 0,0,1,0 },{ 0,0,1,0 },{ -1,0,0,0 } },	//3
-	{ { -1,0,0,0 },{ -1,0,1,0 },{ 0,0,1,0 },{ -1,0,1,0 } },	//4
-	{ { -1,0,1,0 }},	//5
-};
-int dy[6][4][4]{	//몇번 카메라가 어느방향을 보고있을 때 y(상:0, 우:1, 하:2, 좌:3)
-	{ { 0 } },
-	{ { 0,0,0,0 },{ 0,1,0,0 },{ 0,0,0,0 },{ 0,0,0,-1 } },	//1
-	{ { 0,1,0,-1 },{ 0,0,0,0 } },	//2
-	{ { 0,1,0,0 },{ 0,1,0,0 },{ 0,0,0,-1 },{ 0,0,0,-1 } },	//3
-	{ { 0,1,0,-1 },{ 0,1,0,0 },{ 0,1,0,-1 },{ 0,0,0,-1 } },	//4
-	{ { 0,1,0,-1 } },	//5
-};
+int n,m;
 int a[8][8];
-int save[8][8];
+int visited[8][8];
+int ans=64;
 
-int ans;
-int check(){
-	memcpy(save, a, sizeof(save));
-	for (int i = 0; i < tvs.size(); i++) {
-		int x = tvs[i].x;
-		int y = tvs[i].y;
-		save[x][y] = 9;
-		int num = tvs[i].num;
-		int d = tvs[i].dir;
-		for (int k = 0; k < 4; k++) {
-			if (dx[num][d][k] == 0 && dy[num][d][k] == 0) {
-				continue;
-			}
-			int nx = x + dx[num][d][k];
-			int ny = y + dy[num][d][k];
-			while (nx >= 0 && nx < n&&ny >= 0 && ny < m&&a[nx][ny] != 6) {
-				save[nx][ny] = 9;
-				nx += dx[num][d][k];
-				ny += dy[num][d][k];
+int dx[6][4][4]{	//[몇번 카메라][방향][[상하좌우] (상:0, 우:1, 하:2, 좌:3)
+	{ {0} },
+	{ { -1},{ 0 },{1 },{ 0 }},								//1
+	{ { 0,0},{ -1,1}},										//2
+	{ { -1,0},{ 1,0},{ 1,0},{ -1,0 } },						//3
+	{ { -1,0,0 },{ -1,0,1 },{ 0,0,1 },{ -1,0,1 } },			//4
+	{ { -1,0,1,0 }},										//5
+};
+int dy[6][4][4]{	//[몇번 카메라][방향][[상하좌우] (상:0, 우:1, 하:2, 좌:3)
+	{ { 0 } },
+	{ { 0 },{ 1},{ 0 },{ -1 } },							//1
+	{ { -1,1 },{ 0,0} },									//2
+	{ { 0,1 },{ 0,1 },{ 0,-1},{ 0,-1 } },					//3
+	{ { 0,-1,1 },{ 0,1,0 },{ -1,1,0 },{ 0,-1,0 } },			//4
+	{ { 0,1,0,-1 } },										//5
+};
+int rSize[6]={0,4,2,4,4,1};		//회전 횟수
+int dSize[6]={0,1,2,2,3,4};		//방향 횟수
+
+void run(){
+	int x,y,type,dir;
+	memset(visited,0,sizeof(visited));
+	for(int i=0;i<v.size();++i){
+		x=v[i].x;
+		y=v[i].y;
+		type=v[i].type;
+		dir=v[i].dir;
+		for(int k=0;k<dSize[type];++k){
+			int nx=x;
+			int ny=y;
+			while(SAFE(nx,ny)&&a[nx][ny]!=6){
+				visited[nx][ny]=1;
+				nx+=dx[type][dir][k];
+				ny+=dy[type][dir][k];
 			}
 		}
 	}
-	int cnt = 0;
-	//printf("\n");
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			//printf("%d ", save[i][j]);
-			if (save[i][j] ==0) {
-				cnt += 1;
-			}
-		}
-		//printf("\n");
-	}
-	return cnt;
 }
-void simulate(int i) {
-	if (i == tvs.size()) {
-		int t = check();
-		ans = t < ans ? t : ans;
+void dfs(int idx){
+	if(idx==v.size()){
+		run();
+		int cnt=0;
+		for(int i=0;i<n;++i){
+			for(int j=0;j<m;++j){
+				if(!visited[i][j]&&a[i][j]!=6){
+					cnt++;
+				}
+			}
+		}
+		ans=min(ans,cnt);
 		return;
 	}
-	for (int k = 0; k < 4; k++) {
-		if ((tvs[i].num == 2&&k>2)||(tvs[i].num==5&&k>1)) {
-			continue;
-		}
-		tvs[i].dir = k;
-		simulate(i + 1);
+	int t=v[idx].type;
+	for(int i=0;i<rSize[t];++i){
+		dfs(idx+1);
+		v[idx].dir=(v[idx].dir+1)%rSize[t];
 	}
-	return;
 }
+
 int main()
 {
-	scanf("%d %d", &n, &m);
-	ans = n * m;
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			scanf("%d", &a[i][j]);
-			if (a[i][j] >= 1 && a[i][j] <= 5) {
-				tvs.push_back({ i,j,a[i][j],0 });
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cin>>n>>m;
+	for(int i=0;i<n;++i){
+		for(int j=0;j<m;++j){
+			cin>>a[i][j];
+			if(a[i][j]>=1&&a[i][j]<=5){
+				v.emplace_back(i,j,a[i][j],0);
 			}
 		}
 	}
 
-	simulate(0);
-	printf("%d", ans);
-
+	dfs(0);
+	cout<<ans;
 	return 0;
 }
