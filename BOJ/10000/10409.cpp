06b70310ef0f972093cf0428dc17305016e18d1a
nsj6646 <nsj6646@gmail.com>
2019-05-26 16:32:49 +0900

    boj 10409: 서버

--- BOJ/10000/10409.cpp
+++ BOJ/10000/10409.cpp
@@ -1,27 +1,27 @@
-/*
- * boj 10409: 서버
- * copyright: nsj6646@naver.com
- */
-#include <bits/stdc++.h>
-using namespace std;
+    /*
+    * boj 10409: 서버
+    * copyright: nsj6646@naver.com
+    */
+    #include <bits/stdc++.h>
+    using namespace std;
 
-int a[51];
-int main()
-{
-    ios_base::sync_with_stdio(false);
-    cin.tie(0);
-    freopen("input.txt","r",stdin);
-    int n,T;
-    cin>>n>>T;
-    int ans=0;
-    for(int i=1;i<=n;++i){
-        cin>>a[i];
-        a[i]+=a[i-1];
-        if(a[i]<=T) ans=i;
+    int a[51];
+    int main()
+    {
+        ios_base::sync_with_stdio(false);
+        cin.tie(0);
+        freopen("input.txt","r",stdin);
+        int n,T;
+        cin>>n>>T;
+        int ans=0;
+        for(int i=1;i<=n;++i){
+            cin>>a[i];
+            a[i]+=a[i-1];
+            if(a[i]<=T) ans=i;
 
+        }
+        cout<<ans;  
+
+
+        return 0;
     }
-    cout<<ans;  
-
-
-    return 0;
-}
